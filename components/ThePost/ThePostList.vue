81daffe754ec671d29ba2e511c7328630f9fc1ef
Michal Staniecko <michal.staniecko@gmail.com>
2024-06-30 23:51:33 +0200

    refactored main loop

--- components/ThePost/ThePostList.vue
+++ components/ThePost/ThePostList.vue
@@ -14,16 +14,16 @@ defineProps<{
       <div class="grid gap-10">
         <ThePost v-for="post in posts" :key="post.id" :post="post" />
       </div>
+      <div class="text-center py-10">
+        <button
+          :disabled="status === 'pending'"
+          class="bg-primary-500 text-white px-5 py-2 rounded-lg disabled:opacity-50"
+          @click.prevent="handleLoadMore"
+        >
+          Load more
+        </button>
+      </div>
     </template>
-    <div class="text-center py-10">
-      <button
-        :disabled="status === 'pending'"
-        class="bg-primary-500 text-white px-5 py-2 rounded-lg disabled:opacity-50"
-        @click.prevent="handleLoadMore"
-      >
-        Load more
-      </button>
-    </div>
   </div>
 </template>
 
