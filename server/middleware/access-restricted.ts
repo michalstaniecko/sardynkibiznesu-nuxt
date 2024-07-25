f5dac28c9d3335d1ee6b0c7d61fdb34dcff8315a
Michal Staniecko <s.michal@smultron.pl>
2024-07-25 09:14:54 +0200

    fix

--- server/middleware/access-restricted.ts
+++ server/middleware/access-restricted.ts
@@ -1,10 +1,10 @@
-// export default defineEventHandler((event) => {
-//   if (event.path.startsWith("/_ipx")) return;
-//   if (event.path.startsWith("/_vercel")) return;
-//
-//   const config = useRuntimeConfig(event);
-//
-//   const accessRestricted = config.accessRestricted;
-//
-//   setCookie(event, "accessRestricted", accessRestricted ? "1" : "0");
-// });
+export default defineEventHandler((event) => {
+  // if (event.path.startsWith("/_ipx")) return;
+  // if (event.path.startsWith("/_vercel")) return;
+  //
+  // const config = useRuntimeConfig(event);
+  //
+  // const accessRestricted = config.accessRestricted;
+  //
+  // setCookie(event, "accessRestricted", accessRestricted ? "1" : "0");
+});
