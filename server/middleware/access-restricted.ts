367f51aeba2605f7486cab989e255a4826893b28
Michal Staniecko <s.michal@smultron.pl>
2024-07-25 08:46:03 +0200

    added middleware for pagespeed benchmark

--- server/middleware/access-restricted.ts
+++ server/middleware/access-restricted.ts
@@ -1,15 +1,9 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler((event) => {
   if (event.path.startsWith("/_ipx")) return;
 
   const config = useRuntimeConfig(event);
-  const requestURL = getRequestURL(event);
 
   const accessRestricted = config.accessRestricted;
-
-  if (requestURL.searchParams.get("pagespeed") === "1") {
-    deleteCookie(event, "accessRestricted");
-    return;
-  }
 
   setCookie(event, "accessRestricted", accessRestricted ? "1" : "0");
 });
