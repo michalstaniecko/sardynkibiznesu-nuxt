17b7abc5b2ef6eecce3d60209682e183660c9966
Michal Staniecko <s.michal@smultron.pl>
2024-07-25 09:12:36 +0200

    disabled access restricted

--- middleware/access-restricted.global.ts
+++ middleware/access-restricted.global.ts
@@ -1,29 +1,29 @@
-export default defineNuxtRouteMiddleware((to) => {
-  const hasAccess = useCookie("hasAccess");
-  const accessRestricted = useCookie<number>("accessRestricted");
-
-  if (to.query.pagespeed === "1") {
-    accessRestricted.value = 0;
-  }
-
-  const hasAccessOrNotRestricted = hasAccess.value || accessRestricted.value !== 1;
-
-  const isClient = import.meta.client;
-  const isServer = import.meta.server;
-
-  if (isClient) {
-    if (!hasAccessOrNotRestricted && to.path !== "/auth") {
-      return abortNavigation();
-    }
-  }
-
-  if (isServer) {
-    if (!hasAccessOrNotRestricted && to.path !== "/auth") {
-      return navigateTo("/auth");
-    }
-
-    if (hasAccessOrNotRestricted && to.path === '/auth') {
-      return abortNavigation();
-    }
-  }
-})
+// export default defineNuxtRouteMiddleware((to) => {
+//   const hasAccess = useCookie("hasAccess");
+//   const accessRestricted = useCookie<number>("accessRestricted");
+//
+//   if (to.query.pagespeed === "1") {
+//     accessRestricted.value = 0;
+//   }
+//
+//   const hasAccessOrNotRestricted = hasAccess.value || accessRestricted.value !== 1;
+//
+//   const isClient = import.meta.client;
+//   const isServer = import.meta.server;
+//
+//   if (isClient) {
+//     if (!hasAccessOrNotRestricted && to.path !== "/auth") {
+//       return abortNavigation();
+//     }
+//   }
+//
+//   if (isServer) {
+//     if (!hasAccessOrNotRestricted && to.path !== "/auth") {
+//       return navigateTo("/auth");
+//     }
+//
+//     if (hasAccessOrNotRestricted && to.path === '/auth') {
+//       return abortNavigation();
+//     }
+//   }
+// })
